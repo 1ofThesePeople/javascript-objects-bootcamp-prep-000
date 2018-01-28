78892edb3986bf9351adc5fb4f0accfa944d8961
Jeremy Che <jeremy.che48@gmail.com>
2018-01-28 09:10:25 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
 var playlist = {Ryan: "She Won\'t Wait"}
 
 function updatePlaylist(playlist, artist, song) {
-  return Object.assign({}, playlist, {[artist]: [song]})
+  var addition = {[artist]: song}
+  return Object.assign({}, playlist, addition)
 }

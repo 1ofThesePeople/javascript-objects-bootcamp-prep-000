2779cdddd17c356f2a1e2c600da5519fbc424a77
Jeremy Che <jeremy.che48@gmail.com>
2018-01-28 09:14:27 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,11 @@ function updatePlaylist(playlist, artist, song) {
   var addition = {[artist]: song}
   return Object.assign({}, playlist, addition)
 }
+
+function removeFromPlaylist(playlist, artist) {
+  if delete playlist[artist] {
+    return playlist
+  } else {
+    return false
+  }
+}
